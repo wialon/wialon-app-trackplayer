b7b3c2020755003d50329addf962bf7ba397a552
Mimicar <mimicar@mimicar.sk>
2014-03-17 13:51:21 +0100

    v1.0

---
+++ lang/sk.js
@@ -0,0 +1,215 @@
+TRANSLATIONS = {
+	"Track player": "Prehrávanie trasy",
+
+	"Monitoring info": "",
+
+	"Interval": "",
+
+	"Today": "Dnes",
+
+	"Yesterday": "Včera",
+
+	"Current week": "Aktuálny týždeň",
+
+	"Current month": "Aktuálny mesiac",
+
+	"7 days": "7 dn",
+
+	"30 days": "30 dn",
+
+	"Custom interval": "Vlastný interval",
+
+	"Unit": "Jednotka",
+
+	"Change interval": "Zmeniť interval",
+
+	"Show track": "Zobraziť trasu",
+
+	"Tracks": "Trasy",
+
+	"Playback speed": "Rýchlosť prehrávania",
+
+	"Rotate icon": "Otáčať ikonu",
+
+	"Double-click on necessary sensors below to track them.": "Dvojklikom pridajte nižšie uvedené senzory, ktoré chcete sledovať",
+
+	"General information": "Všeobecné informácie",
+
+	"Hide settings": "Skryť nastavenie",
+
+	"Sensors": "Senzory",
+
+	"Skip intervals between trips": "Skryť intervaly medzi trasami",
+
+	"Prev": "Predchádzajúci",
+
+	"Next": "Ďaľší",
+
+	"Login error, restart the application": "Chyba prihlásenia, reštartujte aplikáciu",
+
+	"Invalid time interval": "Neplatný časový interval",
+
+	"Select unit": "Vyberte jednotku",
+
+	"There is a track for this unit already": "Trasa pre danú jednotku už existuje",
+
+	"No messages for the selected interval": "Žiadne správy pre zvolený interval",
+
+	"Unable to build a track": "Nie je možné vytvoriť trasu",
+
+	"Unable to load trips": "Nie je možné načítať trasy",
+
+	"Session error, restart the application": "Chyba, reštartujte aplikáciu",
+
+	"Init messages error": "Chyba správ",
+
+	"Nothing to delete, no tracks for the given unit": "Nemožno odstrániť, žiadne trasy pre danú jednotku",
+
+	"Hide/show track": "Skryť / zobraziť trasu",
+
+	"Focus on unit": "Zameranie na jednotku",
+
+	"Delete track": "Vymazať trasu",
+
+	"Settings": "Nastavenie",
+
+	"Follow the unit on map": "Sledovať jednotku na mape",
+
+	"Show/hide pictures": "Skryť / zobraziť obrázok",
+
+	"Show/hide parameters": "Skryť / zobraziť parametre",
+
+	"Speed": "Rýchlosť",
+
+	"mph": "",
+
+	"kph": "",
+
+	"miles": "míle",
+
+	"km": "km",
+
+	"Go to the first message": "Prejsť na prvú správu",
+
+	"Beginning": "Začiatok",
+
+	"Go to the last message": "Prejsť na poslednú správu",
+
+	"End": "Koniec",
+
+	"Unable to load events": "Nie je možné načítať udalosti",
+
+	"%d events have been found for %s. First 1000 of them will be shown.\nTo see other events, change time interval.": "% udalostí boli nájdené pre %. Prvých 1000 z nich bude zobrazených.\nAk chcete zobraziť ďalšie akcie , zmeňte časový interval.",
+
+	"January": "Január",
+	"February": "Február",
+	"March": "Marec",
+	"April": "Apríl",
+	"May": "Máj",
+	"June": "Jún",
+	"July": "Júl",
+	"August": "August",
+	"September": "September",
+	"October": "Október",
+	"November": "November",
+	"December": "December",
+
+	"Jan": "",
+	"Feb": "",
+	"Mar": "",
+	"Apr": "",
+	"Jun": "",
+	"Jul": "",
+	"Aug": "",
+	"Sep": "",
+	"Oct": "",
+	"Nov": "",
+	"Dec": "",
+
+	"Sunday": "Nedeľa",
+	"Monday": "Pondelok",
+	"Tuesday": "Utorok",
+	"Wednesday": "Streda",
+	"Thursday": "Štvrtok",
+	"Friday": "Piatok",
+	"Saturday": "Sobota",
+
+	"Sun": "Ne",
+	"Mon": "Po",
+	"Tue": "Ut",
+	"Wed": "St",
+	"Thu": "Št",
+	"Fri": "Pi",
+	"Sat": "So",
+
+	"Su": "Ne",
+	"Mo": "PO",
+	"Tu": "Ut",
+	"We": "St",
+	"Th": "Št",
+	"Fr": "Pi",
+	"Sa": "So",
+
+	"Time": "Čas",
+	"Hours": "Hodiny",
+	"Minutes": "Minúty",
+	"Seconds": "Sekundy",
+
+	//Sensors
+	"Absolute engine hours": "Absolútne motohodiny",
+	"Absolute fuel consumption sensor": "Absolútny senzor merania spotreby",
+	"Accelerometer": "",
+	"Counter sensor": "",
+	"Custom sensor": "Vlastný senzor",
+	"State sensor": "",
+	"Custom digital sensor": "Vlastný digitálny senzor",
+	"Driver binding": "Pripojenie vodiča",
+	"Engine efficiency sensor": "",
+	"Engine ignition sensor": "Senzor štartu motora",
+	"Engine revs sensor": "",
+	"Equipment binding": "",
+	"Fuel level sensor": "Senzor hladiny paliva",
+	"hours": "hodiny",
+	"Impulse fuel consumption sensor": "",
+	"Fuel level impulse sensor": "Impulzný senzor hladiny paliva",
+	"Instant fuel consumption sensor": "",
+	"litres": "litre",
+	"m": "",
+	"Mileage sensor": "Senzor vzdialenosti",
+	"Relative odometer": "Relatívny odometer",
+	"On/Off": "",
+	"Relative engine hours": "",
+	"rpm": "ot/min",
+	"Enter sensor name, type, description or parameter.": "Zadajte meno, typ, popis alebo parameter senzora",
+	"Temperature coefficient": "Koeficient teploty",
+	"Temperature sensor": "Senzor teploty",
+	"Trailer binding": "Pripojenie prívesu",
+	"V": "",
+	"Voltage sensor": "Senzor napätia",
+	"on": "",
+	"off": "",
+
+	//Messages
+	"GPRS traffic counter value: %d KB.": "",
+	"GPRS traffic counter reset. %d KB consumed.": "",
+	"Engine hours counter value is %d h.": "",
+	"Engine hours counter value was changed from %d h to %d h.": "",
+	"Mileage counter value is %d km.": "",
+	"Mileage counter value was changed from %d km to %d km.": "",
+	"Route '%s': round aborted.": "",
+	"Route '%s': arrival at point '%s'.": "",
+	"Route '%s': departure from point '%s'.": "",
+	"Route '%s': round finished.": "",
+	"Route '%s': unit returned to schedule.": "",
+	"Route '%s': point '%s' skipped.": "",
+	"Route '%s': round by schedule '%s' started.": "",
+	"Route '%s': unit is ahead of schedule.": "",
+	"Route '%s': unit is late.": "",
+	"Route %s: entered %s": "",
+	"Route %s: left %s": "",
+	"Route": "",
+	"Event": "",
+	"Filling": "",
+	"Maintenance": "",
+	"Violation": ""
+};
